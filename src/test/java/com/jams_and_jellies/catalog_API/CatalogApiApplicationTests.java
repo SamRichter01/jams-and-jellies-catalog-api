f7cc9d90868e5b4bbbc1a886898543ddd2e9b210
RICH9794 <rich9794@stthomas.edu>
2025-02-04 11:50:25 -0600

    Added boilerplate spring project

---
+++ src/test/java/com/jams_and_jellies/catalog_API/CatalogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jams_and_jellies.catalog_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
