4ec92b54d062ce2475d811bdb1bc9f9e664fc3db
RICH9794 <rich9794@stthomas.edu>
2025-02-11 13:00:16 -0600

    beginning database implementation

--- src/main/java/com/jams_and_jellies/catalog_API/CatalogApiApplication.java
+++ src/main/java/com/jams_and_jellies/catalog_API/CatalogApiApplication.java
@@ -2,8 +2,9 @@ package com.jams_and_jellies.catalog_API;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.r2dbc.R2dbcAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {R2dbcAutoConfiguration.class})
 public class CatalogApiApplication {
 
 	public static void main(String[] args) {
