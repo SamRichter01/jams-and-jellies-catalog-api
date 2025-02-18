bd1d1486de91fbf7874e21c11941fb0e52579fb9
RICH9794 <rich9794@stthomas.edu>
2025-02-18 12:37:50 -0600

    Getting close to database functionality

--- src/main/java/com/jams_and_jellies/catalog_API/GreetingHandler.java
+++ src/main/java/com/jams_and_jellies/catalog_API/GreetingHandler.java
@@ -1,22 +1,30 @@
 package com.jams_and_jellies.catalog_API;
 
-import org.hibernate.reactive.mutiny.Mutiny;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
+
+import org.hibernate.reactive.mutiny.Mutiny;
+import org.hibernate.Session;
+import io.smallrye.mutiny.Uni;
 import jakarta.persistence.*;
 import reactor.core.publisher.Mono;
 
 @Component
 public class GreetingHandler {
 
-    // Creating the entity manager for hibernate
-    EntityManagerFactory emf = Persistence.createEntityManagerFactory("jelly-postgresql");
-    Mutiny.SessionFactory sessionFactory = emf.unwrap(Mutiny.SessionFactory.class);
-
     public Mono<ServerResponse> hello(ServerRequest request) {
+
+        // Creating the entity manager for hibernate
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("jelly-postgresql");
+        Mutiny.SessionFactory sessionFactory = emf.unwrap(Mutiny.SessionFactory.class);
+    
+        Product jam = new Product("Grape");
+    
+        sessionFactory.withTransaction((session, tx) -> session.persist(jam));
+
         return ServerResponse.ok().contentType(MediaType.APPLICATION_JSON)
             .body(BodyInserters.fromValue(new Greeting("Hello, Spring!")));
     }
